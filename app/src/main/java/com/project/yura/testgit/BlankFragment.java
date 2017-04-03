89df1bf7da328172db921847bcfa7a7da18d7038
Yura <yurii.polyniak@gmail.com>
2017-04-03 22:13:40 +0300

    add fragment to activity

--- app/src/main/java/com/project/yura/testgit/BlankFragment.java
+++ app/src/main/java/com/project/yura/testgit/BlankFragment.java
@@ -1,9 +1,9 @@
 package com.project.yura.testgit;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,9 +46,9 @@ public class BlankFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_blank, container, false);
     }
 
-    public void onButtonPressed(Uri uri) {
+    public void onButtonPressed(String message) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onFragmentInteraction(message);
         }
     }
 
@@ -71,6 +71,6 @@ public class BlankFragment extends Fragment {
 
 
     public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction(String message);
     }
 }
--- app/src/main/java/com/project/yura/testgit/MainActivity.java
+++ app/src/main/java/com/project/yura/testgit/MainActivity.java
@@ -1,13 +1,25 @@
 package com.project.yura.testgit;
 
+import android.app.Fragment;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements BlankFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment, BlankFragment.newInstance("Text1", "Text2"))
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(String message) {
+        
     }
 }
